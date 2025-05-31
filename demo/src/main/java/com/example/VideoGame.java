325b10d91929f36dc9309fc13104bb8f8eb4fadd
DiegoGonzalez09 <gon241214@uvg.edu.gt>
2025-05-31 10:09:37 -0600

    Se actualizo la clase de Videojuego

--- demo/src/main/java/com/example/VideoGame.java
+++ demo/src/main/java/com/example/VideoGame.java
@@ -4,71 +4,62 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class VideoGame {
-    private String id;
-    private String nombre;
-    private int anioLanzamiento;
+    private String titulo;
+    private int maxJugadores;
+    private boolean multiplataforma;
     private Set<String> generos;
-    private Set<String> plataformas;
-    private Set<String> desarrolladores;
-    private boolean esMultijugador;
+    private String publicador;
+    private double puntajeCritica;
+    private double ventas;
+    private String consola;
+    private String rating;
+    private int anioLanzamiento;
     
-    public VideoGame(String id, String nombre, int anioLanzamiento) {
-        this.id = id;
-        this.nombre = nombre;
+    public VideoGame(String titulo, int anioLanzamiento, int maxJugadores, boolean multiplataforma, String publicador, double puntajeCritica, double ventas, String consola, String rating){ 
+        this.titulo = titulo;
         this.anioLanzamiento = anioLanzamiento;
         this.generos = new HashSet<>();
-        this.plataformas = new HashSet<>();
-        this.desarrolladores = new HashSet<>();
-        this.esMultijugador = false;
+        this.maxJugadores = maxJugadores;
+        this.multiplataforma = multiplataforma;
+        this.publicador = publicador;
+        this.puntajeCritica = puntajeCritica;
+        this.ventas = ventas;
+        this.consola = consola;
+        this.rating = rating;
     }
     
-    // Getters y setters
-    public String getId() {return id;}
-    public void setId(String id) {this.id = id;}
-    
-    public String getNombre() {return nombre;}
-    public void setNombre(String nombre) {this.nombre = nombre;}
-    
+    public String getTitulo() { return titulo; }
+    public int getMaxJugadores() { return maxJugadores; }
+    public boolean isMultiplataforma() { return multiplataforma; }
+    public Set<String> getGeneros() { return generos; }
+    public String getPublicador() { return publicador; }
+    public double getPuntajeCritica() { return puntajeCritica; }
+    public double getVentas() { return ventas; }
+    public String getConsola() { return consola; }
     public int getAnioLanzamiento() {return anioLanzamiento;}
-    public void setAnioLanzamiento(int anioLanzamiento) {this.anioLanzamiento = anioLanzamiento;}
-    
-    public Set<String> getGeneros() {return generos;}
-    public void addGenero(String genero) {this.generos.add(genero);}
-    
-    public Set<String> getPlataformas() {return plataformas;}
-    public void addPlataforma(String plataforma) {this.plataformas.add(plataforma);}
-    
-    public Set<String> getDesarrolladores() {return desarrolladores;}
-    public void addDesarrollador(String desarrollador) {this.desarrolladores.add(desarrollador);}
-    
-    public boolean isEsMultijugador() {return esMultijugador;}
-    public void setEsMultijugador(boolean esMultijugador) {this.esMultijugador = esMultijugador;}
+    public String getRating() { return rating; }
+
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+    public void setMaxJugadores(int maxJugadores) { this.maxJugadores = maxJugadores; }
+    public void setMultiplataforma(boolean multiplataforma) { this.multiplataforma = multiplataforma;}
+    public void setGeneros(Set<String> generos){this.generos = generos;}
+    public void setPublicador(String publicador) { this.publicador = publicador; }
+    public void setPuntajeCritica(double puntajeCritica) { this.puntajeCritica = puntajeCritica;}
+    public void setVentas(double ventas) { this.ventas = ventas; }
+    public void setConsola(String consola) { this.consola = consola; }
+    public void setAnioLanzamiento(int anioLanzamiento){this.anioLanzamiento = anioLanzamiento;}
+    public void setRating(String rating) { this.rating = rating; }
     
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        
         VideoGame videoGame = (VideoGame) o;
-        return id.equals(videoGame.id);
+        return titulo.equals(videoGame.titulo);
     }
     
     @Override
     public int hashCode() {
-        return id.hashCode();
-    }
-    
-    @Override
-    public String toString() {
-        return "VideoGame{" +
-                "id='" + id + '\'' +
-                ", nombre='" + nombre + '\'' +
-                ", anioLanzamiento=" + anioLanzamiento +
-                ", generos=" + generos +
-                ", plataformas=" + plataformas +
-                ", desarrolladores=" + desarrolladores +
-                ", esMultijugador=" + esMultijugador +
-                '}';
+        return titulo.hashCode();
     }
 }
-
